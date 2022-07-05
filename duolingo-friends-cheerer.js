570ce3a6937c96d1a9a5d250efddd3d430aab802
Temesky <16792417+teamugcode@users.noreply.github.com>
2022-07-05 19:40:47 +0300

    Add some debug messages of browser steps

--- duolingo-friends-cheerer.js
+++ duolingo-friends-cheerer.js
@@ -11,6 +11,7 @@ const app = express();
  * Address http://localhost:4000/cheer-friends?url=https://www.duolingo.com
  */
 app.get("/cheer-friends", async function (req, res) {
+    console.log("starting browser");
     // Start browser
     const browser = await puppeteer.launch();
 
@@ -20,9 +21,11 @@ app.get("/cheer-friends", async function (req, res) {
     // Set custom viewport size for screenshot (and maybe for easier navigation)
     await page.setViewport({ width: 1920, height: 1250 });
 
+    console.log("navigating to page");
     // Pass url param from client side ?url=https://www.duolingo.com
     await page.goto(req.query.url);
 
+    console.log("starting to capture a screenshot");
     // Start capturing a screenshot
     const screenshotBuffer = await page.screenshot();
 
@@ -35,6 +38,7 @@ app.get("/cheer-friends", async function (req, res) {
     // End responce process
     res.end(screenshotBuffer);
 
+    console.log("closing browser");
     // Close browser
     await browser.close();
 });
