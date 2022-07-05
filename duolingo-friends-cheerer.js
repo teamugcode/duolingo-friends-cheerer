d7c9c8b120e3cf7fad3ca63077632ee8399917ca
Temesky <16792417+teamugcode@users.noreply.github.com>
2022-07-05 19:20:01 +0300

    Set express app to open browser with puppeteer, navigate to duolingo website and take a screenshot

--- duolingo-friends-cheerer.js
+++ duolingo-friends-cheerer.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const puppeteer = require("puppeteer");
 
 const app = express();
 
-// http://localhost:4000/cheer-friends
-app.get("/cheer-friends", function (req, res) {
-    res.send("Hello World");
+/**
+ * Run this server on command line:
+ * node duolingo-friends-cheerer.js
+ *
+ * Express app to run server
+ * Address http://localhost:4000/cheer-friends?url=https://www.duolingo.com
+ */
+app.get("/cheer-friends", async function (req, res) {
+    // Start browser
+    const browser = await puppeteer.launch();
+
+    // Start new page
+    const page = await browser.newPage();
+
+    // Set custom viewport size for screenshot (and maybe for easier navigation)
+    await page.setViewport({ width: 1920, height: 1250 });
+
+    // Pass url param from client side ?url=https://www.duolingo.com
+    await page.goto(req.query.url);
+
+    // Start capturing a screenshot
+    const screenshotBuffer = await page.screenshot();
+
+    // Respond with image
+    res.writeHead(200, {
+        "Content-Type": "image/png",
+        "Content-Length": screenshotBuffer.length,
+    });
+
+    // End responce process
+    res.end(screenshotBuffer);
+
+    // Close browser
+    await browser.close();
 });
 
 app.listen(4000);
