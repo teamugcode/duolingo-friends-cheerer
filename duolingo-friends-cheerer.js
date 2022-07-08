4821f6713be1b0b5dadad5f7b6cc7713102d00a5
Temesky <16792417+teamugcode@users.noreply.github.com>
2022-07-08 18:56:32 +0300

    Add functionality to reveal login form

--- duolingo-friends-cheerer.js
+++ duolingo-friends-cheerer.js
@@ -25,6 +25,31 @@ app.get("/cheer-friends", async function (req, res) {
     // Pass url param from client side ?url=https://www.duolingo.com
     await page.goto(req.query.url);
 
+    // Wait for already have account button selector
+    await page.waitForFunction(() => {
+        const selectors = ['button[data-test="have-account"]'];
+        return selectors.every(
+            (selector) => document.querySelector(selector)?.clientHeight > 0
+        );
+    });
+
+    // Add debug message if have account selector exists
+    if ((await page.$('button[data-test="have-account"]')) !== null) {
+        console.log("have account button selector found");
+    } else {
+        console.log("have account button selector not found");
+    }
+
+    // Focus and click have account button
+    await page.focus('button[data-test="have-account"]');
+    await page.click('button[data-test="have-account"]');
+
+    // Add debug message if we reach until the click
+    console.log("should have clicked");
+
+    // Wait short time when the (react) page updates
+    await page.waitForTimeout(500);
+
     console.log("starting to capture a screenshot");
     // Start capturing a screenshot
     const screenshotBuffer = await page.screenshot();
